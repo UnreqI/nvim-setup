8ff4467e3114b87a219ce69738889a0a24311b6d
Unreal <81265153+UnreqI@users.noreply.github.com>
2025-09-05 19:29:15 +0100

    Add tokyonight, onedark and monokai themes

---
+++ lua/plugins/themes/monokai.lua
@@ -0,0 +1,7 @@
+return {
+    "tanvirtin/monokai.nvim",
+    name = "monokai",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+}
---
+++ lua/plugins/themes/onedark.lua
@@ -0,0 +1,7 @@
+return {
+    "navarasu/onedark.nvim",
+    name = "onedark",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+}
